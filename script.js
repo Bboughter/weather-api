7bbc05260891f3f26f6fe6db6e7b0674c8730e60
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-29 22:29:41 -0700

    added temp, humidity, and wind speed to be added to page

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ searchBtn.click(function() {
     var searchInput = $('.form-control');
     var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIkey + "&units=imperial";
     var fiveDayForcast = "api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + APIkey + "&units=imperial";
-     if searchInput == "") {
-        console.log(searchInput);
+     if searchInput == "") { 
+         console.log(searchInput);
      } else {
         $.ajax({
             url: currentWeather,
@@ -24,6 +24,24 @@ searchBtn.click(function() {
             cityName.apped("<li>" + response.name + "</li>")
             var local = localStorage.setItem(count, response.name);
             count = count + 1;
+
+            var currentCard = $(".currentCard").append("<div>").addClass("card-body");
+            currentCard.empty();
+            var currentName = currentCard.append("<p>");
+            currentCard.append(currentName);
+
+            var timeUTC = new Date(response.dt * 1000);
+            currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
+            currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+    
+            var currentTemp = currentName.append("<p>");
+        
+            currentName.append(currentTemp);
+            currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+            
+            currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+    
+            currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
         })
      }
     });
