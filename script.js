33ed5789c1b0d7fd1d307eb48c8760da0c9d82fd
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-29 22:24:56 -0700

    added search with API key to get the current weather

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ for (var i = 0; i < localStorage.length; i++) {
     var cityName = $(".cities-searched").addclass("cities");
     cityName.apped("<li>" + city + "</li>")
 }
+
+var count = 0;
+
+searchBtn.click(function() {
+    var searchInput = $('.form-control');
+    var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIkey + "&units=imperial";
+    var fiveDayForcast = "api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + APIkey + "&units=imperial";
+     if searchInput == "") {
+        console.log(searchInput);
+     } else {
+        $.ajax({
+            url: currentWeather,
+            method: "GET"
+        }).then (function(response) {
+            var cityName = $(".cities-searched").addclass("cities");
+            cityName.apped("<li>" + response.name + "</li>")
+            var local = localStorage.setItem(count, response.name);
+            count = count + 1;
+        })
+     }
+    });
