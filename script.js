8dc3515e219a98bcc9eb84ceb2ee5b8d458a4b12
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-29 22:05:29 -0700

    added function for retrieving info from local storage

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var APIkey = '792acd53a4130029a12f98144cca09f3'
-var city;
+var searchBtn = $(".btn");
+
+for (var i = 0; i < localStorage.length; i++) {
+    var city = localStorage.getItem(i);
+    var cityName = $(".cities-searched").addclass("cities");
+    cityName.apped("<li>" + city + "</li>")
+}
