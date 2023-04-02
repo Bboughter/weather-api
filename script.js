d8c0841f633536ef1c279fd1e542e49675b6e0ee
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-01 22:06:32 -0700

    changes to HTML and Javascript to fix info showing on page

--- script.js
+++ script.js
@@ -3,7 +3,48 @@ var searchBtn = $(".btn");
 var searchInput = document.querySelector('.search')
 
 searchBtn.on('click', getCurrentWeatherAPI)
-function getCurrentWeatherAPI(queryCurrentWeather) {
+
+
+function displayFiveDayForecast(card, date, temp, wind, humidity){
+    var dateCurrent = dayjs.unix(date).format('MM/DD/YYYY');
+   
+    var currentDate = card.querySelector('.date')
+    var currentTemp = card.querySelector('.temperature')
+
+    currentDate.innerHTML=currentDate.innerHTML + ' ' + dateCurrent
+    currentTemp.innerHTML=currentTemp.innerHTML + ' ' + temp
+}
+
+function getFiveDayWeatherData() {
+
+    var queryFiveDayWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput.value + "&appid=" + APIkey + "&units=imperial";
+    
+    fetch(queryFiveDayWeather)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(weatherData) {
+        var data = weatherData.list
+        var fiveDayCards = document.querySelectorAll('.day-forecast')
+        console.log(fiveDayCards)
+        var index = 0
+        for(var i =0; i <=32; i = i+8){
+            console.log(2)
+            var unixDate = data[i].dt
+            var temp = data[i].main.temp
+            var humidity = data[i].main.humidity
+            var wind = data[i].wind.speed
+
+            
+            displayFiveDayForecast(fiveDayCards[index], unixDate, temp, wind,humidity)
+            index = index + 1
+        }
+      });
+  };
+
+
+function getCurrentWeatherAPI(event) {
+    event.preventDefault();
 
     var queryCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid=" + APIkey + "&units=imperial";
     
@@ -15,34 +56,57 @@ function getCurrentWeatherAPI(queryCurrentWeather) {
         console.log(data);
         var dateCurrent = dayjs.unix(data.dt).format('MM/DD/YYYY');
         var currentWeather = document.querySelector('.current-card');
-        currentWeather.innerHTML='';
+        var currentDate = currentWeather.querySelector('.date')
+        currentDate.innerHTML=currentDate.innerHTML + ' ' + dateCurrent
+        
+        var currentTemp =currentDate
 
-    var currentWeatherCard = document.createElement('div').
-    currentWeatherCard.classList('card');   
-    currentWeatherCard.innerHTML= dateCurrent + '' + "Temperature: " + data.main.temp + "°F" + '' + "Wind: " + data.wind.speed + "MPH" + "" +
-    "Humidity: " + data.main.humidity + "%";
+        getFiveDayWeatherData()
 
-    var weatherContainer = document.createElement('div');
-    weatherContainer.classList.add('weather-container');
-    weatherContainer.appendChild(currentWeatherCard);
-    document.body.appendChild(weatherContainer);
+        // currentWeather.innerHTML='';
+
+    // var currentWeatherCard = document.createElement('div')
+    // currentWeatherCard.classList.add('card')     
+    // currentWeatherCard.innerHTML= dateCurrent + '\n' + "Temperature: " + data.main.temp + "°F" + "Wind: " + data.wind.speed + "MPH" + "" +
+    // "Humidity: " + data.main.humidity + "%";
+
+    // var currentWeatherBody = document.createElement('div')
+    // currentWeatherCard.append(currentWeatherBody)
+    // currentWeather.append(currentWeatherCard)
+ 
     })
 }
 
-// submitBtn.addEventListener("click", function () {
-//     fetch(
-//       "https://api.openweathermap.org/data/2.5/forecast?q=" +
-//         input.value +
-//         "&appid=ce895f0400d0fe16be29264e2d060cb6"
-//     )
-//       .then(function (response) {
+
+
+
+
+
+// searchBtn.on('click', getCurrentWeatherAPI)
+// function getCurrentWeatherAPI(queryCurrentWeather) {
+
+//     var queryCurrentWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput.value + "&appid=" + APIkey + "&units=imperial";
+    
+//     fetch(queryCurrentWeather)
+//     .then(function(response) {
 //         return response.json();
-        
-//       })
-//       .then(function (data) {
+//     })
+//     .then(function(data) {
 //         console.log(data);
-//         var card = document.querySelector(".card-element");
-//         card.innerHTML='';
+//         var dateCurrent = dayjs.unix(data.dt).format('MM/DD/YYYY');
+//         var currentWeather = document.querySelector('.current-card');
+//         currentWeather.innerHTML='';
+
+//     var currentWeatherCard = document.createElement('div')
+//     currentWeatherCard.classList('card')     
+//     currentWeatherCard.innerHTML= dateCurrent + '' + "Temperature: " + data.main.temp + "°F" + '' + "Wind: " + data.wind.speed + "MPH" + "" +
+//     "Humidity: " + data.main.humidity + "%";
+
+//     var currentWeatherBody = document.createElement('div')
+//     currentWeatherCard.append(currentWeatherBody)
+
+ 
+    
   
 //         for (var i = 0; i < data.list.length; i++) {
 //           var fiveDay = dayjs(data.list[i].dt_txt ).format('MM/DD/YYYY');
@@ -50,66 +114,32 @@ function getCurrentWeatherAPI(queryCurrentWeather) {
 //               console.log(data.list[i]);
 //               var cardInfo = document.createElement("div");
 //               cardInfo.classList.add("card");
-//               cardInfo.innerHTML =
-//                 "<div class='card-body'><h5 class='card-title' id='forcastdate'>" +
-//                 fiveDay +
-//                 "</h5><h6 class='card-subtitle mb-2 text-muted'>" +
-//                 "Temperature: " + 
-//                 data.list[i].main.temp + '°F' +
-//                 "<h6 class='card-subtitle mb-2 text-muted'>" +
-//                 "Wind Speed: " +
-//                 data.list[i].wind.speed + 'MPH' +
-//                 "<h6 class='card-subtitle mb-2 text-muted'>" +
-//                 "Humidity: " +
-//                 data.list[i].main.humidity + '%' +
-//                 "<h6></div>";
+//               cardInfo.innerHTML = fiveDay + "Temperature: " + data.list[i].main.temp + '°F' + "Wind Speed: " + data.list[i].wind.speed + 'MPH' + "Humidity: " + data.list[i].main.humidity + '%'
               
-//               card.append(cardInfo);
+            
 //             }
 //         }
 //       });
-//   });
+//   };
   
-//   function localFunction () {
-//   var searchedCity = JSON.parse(localStorage.getItem('searchedCity'));
+// //   function localFunction () {
+// //   var searchedCity = JSON.parse(localStorage.getItem('searchedCity'));
   
-//         if (!searchedCity){
-//         localStorage.setItem('searchedCity', JSON.stringify([input.value]));
-//         }else {
-//         searchedCity.push(input.value);  
-//         localStorage.setItem('searchedCity', JSON.stringify(searchedCity));
-//         }
-//       }
+// //         if (!searchedCity){
+// //         localStorage.setItem('searchedCity', JSON.stringify([input.value]));
+// //         }else {
+// //         searchedCity.push(input.value);  
+// //         localStorage.setItem('searchedCity', JSON.stringify(searchedCity));
+// //         }
+// //       }
   
-//       function createBtn () {
-//         var cityResponse = JSON.parse(localStorage.getItem('searchedCity'));
-//         var searchHistory = document.querySelector(".searchhistory");
-//         for (var i = 0; i < cityResponse.length; i++) {
-//           var searchHistoryBtn = document.createElement("button");
-//           searchHistoryBtn.innerText=cityResponse[i];
-//           searchHistory.append(searchHistoryBtn);
-//         }
-//       }
+// //       function createBtn () {
+// //         var cityResponse = JSON.parse(localStorage.getItem('searchedCity'));
+// //         var searchHistory = document.querySelector(".searchhistory");
+// //         for (var i = 0; i < cityResponse.length; i++) {
+// //           var searchHistoryBtn = document.createElement("button");
+// //           searchHistoryBtn.innerText=cityResponse[i];
+// //           searchHistory.append(searchHistoryBtn);
+// //         }
+// //       }
 
-
-
-// function printCurrentWeather()  {
-
-//     var currentWeatherCard= document.createElement('div');
-    
-//     var currentWeatherBody = document.createElement('div');
-
-//     currentWeatherCard.append(currentWeatherBody);
-
-//     var currentTemp = document.createElement('p');
-//     currentTemp.textContent=data.list[0].main.temp
-
-//     var currentHumidity = document.createElement('p')
-//     currentHumidity.textContent = data.list[0].main.humidity
-
-//     var currentWind = document.createElement('p');
-//     currentWind.textContent =data.list[0].wind.speed
-
-//     currentWeatherBody.append(currentCity, currentTemp, currentHumidity, currentWind)
-//     currentWeather.append(currentWeatherCard)
-// }
